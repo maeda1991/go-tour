f07be03347db8abf83e5438ee2d2bf262adec6e4
maeda1991 <maeda1991@gmail.com>
2015-12-01 13:12:12 +0000

    update

---
+++ stringer.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+func (p Person) String() string {
+	return fmt.Sprintf("%v (%v years)", p.Name, p.Age)
+}
+
+func main() {
+	// String()があれば呼ばれる
+	a := Person{"Arthur Dent", 42}
+	z := Person{"Zaphed Beeblebrox", 9001}
+	fmt.Println(a, z)
+}
